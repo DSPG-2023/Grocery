65480356053efd3ee02d9d93825bce1b28f00ee6
alexcory <alexcory@iastate.edu>
2023-06-05 16:48:06 -0500

    Added primary and secondary shoppers TODO: document

---
+++ R/Primary_Secondary_Shoppers.R
@@ -0,0 +1,11 @@
+
+
+
+
+Primary_Secondary_Shoppers <- function(pop, pct_primary, pct_secondary) {
+  #Takes in pop (calculated by City_Populations), and pct primary and secondary
+  #as defined by the user
+  prim_count = pop * pct_primary
+  secondary_count = pop * pct_secondary
+
+}
