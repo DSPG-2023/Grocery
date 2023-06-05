0763cf0e188ada0060f1b0ff903bca62999a29a4
alexcory <alexcory@iastate.edu>
2023-06-05 14:54:51 -0500

    Renamed quadrant_area to QuadrantArea because we changed how we are naming functions

--- R/QuadrantArea.R
+++ R/QuadrantArea.R
@@ -7,7 +7,7 @@
 #'quadrant_area(user_area = 4)
 #'@export
 
-quadrant_area <- function(user_area) {
+QuadrantArea <- function(user_area) {
 
   output_area <- (pi * (user_area * user_area)) * 1/4
 }
