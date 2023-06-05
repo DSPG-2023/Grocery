b5a1a80c0249809d89e020e0214ed105b05954f6
Aaron N <null94@iastate.edu>
2023-06-05 10:27:49 -0500

    adding details to gross margin

--- R/Gross_Margin.R
+++ R/Gross_Margin.R
@@ -1,6 +1,7 @@
 #'Calculation of Gross Margin
 #'@param Total_Estimated_Revenue Output of a total estimated revenue calculation
 #'@return Outputs Gross Margin (Total Estimated Revenue - Cost of Goods Sold) (in dollars)
+#'@details This function is taken from the "Estimating Expense" excel sheet
 #'@examples
 #'# Gross_Margin(Total_Estimated_Revenue = 2000000)
 #'
@@ -11,4 +12,3 @@ Gross_Margin <- function(Total_Estimated_Revenue) {
   Total_Estimated_Revenue - Goods_Cost
 }
 
-Gross_Margin(3001981)
