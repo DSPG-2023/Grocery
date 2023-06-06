bd87753116264539c97e73044387d679a2ed5e08
srika1919 <128859229+srika1919@users.noreply.github.com>
2023-06-06 13:37:35 -0500

    Rare count added to the function Primary_Secondary_Rare_Shoppers. Fixed the function calling errors in the Total_Estimate_Revenue

--- R/Primary_Secondary_Shoppers.R
+++ R/Primary_Secondary_Shoppers.R
@@ -1,17 +1,19 @@
-#'Calculation for a quarter circle.
+#'Calculation for a shoppers from Primary secondary and rare categories in a Market
 #'
 #'@param pop population inside quadrants, gotten from the City_Populations method
-#'@param pct_primary,pct_secondary percentage of the county that is a primary and secondary shopper
-#'@return sets values for the number of primary and secondary shoppers
+#'@param pct_primary,pct_secondary percentage of the county that is a primary, secondary and rare shopper
+#'@return sets values for the number of primary, secondary, and rare shoppers
 #'
 #'@export
 
 
 
-Primary_Secondary_Shoppers <- function(pop, pct_primary, pct_secondary) {
+Primary_Secondary_Rare_Shoppers <- function(pct_primary, pct_secondary,pct_rare) {
   #Takes in pop (calculated by City_Populations), and pct primary and secondary
   #as defined by the user
+  pop<-City_Populations()
   prim_count = pop * pct_primary
   secondary_count = pop * pct_secondary
+  rare_count=pop*pct_rare
 
 }
