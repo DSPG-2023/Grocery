d9635a6dcf1e731e76c7a0242a9531552ba9770e
alexcory <alexcory@iastate.edu>
2023-06-06 13:29:39 -0500

    Rural Population Function

---
+++ R/Rural_Population.R
@@ -0,0 +1,16 @@
+#'Calculation for a quarter circle.
+#'
+#'@param county_pop the population of the whole county
+#'@param towns_pop the sum of all the towns populations in the county
+#'@param pct_county the percent of the county in the area
+#'@return outputs the rural population
+#'
+#'@export
+
+
+
+Rural_Population(county_pop, towns_pop, pct_county) {
+  #This function subtracts the sum of all the towns populations from the county and multiplies it
+  #by the percent of the county that the area covers to find the # of rural people there are
+  return((county_pop - towns_pop)*pct_county)
+}
