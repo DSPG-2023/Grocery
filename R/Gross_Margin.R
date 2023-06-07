465f1e0835d93efa3699337245a406e4fa779709
Aaron N <null94@iastate.edu>
2023-06-07 14:15:00 -0500

    Gross margin update

--- R/Gross_Margin.R
+++ R/Gross_Margin.R
@@ -1,14 +1,24 @@
 #'Calculation of Gross Margin
 #'@param Total_Estimated_Revenue Output of a total estimated revenue calculation
-#'@return Outputs Gross Margin (Total Estimated Revenue - Cost of Goods Sold) (in dollars)
-#'@details This function is taken from the "Estimating Expense" excel sheet
+#'@return Outputs Gross Margin depending on which one of the 4 revenue ranges
+#'the input "Total_Estimated_Revenue" falls into (in dollars)
+#'@details This function is taken from the "Estimating Expense" excel sheet (step six)
 #'@examples
 #'# Gross_Margin(Total_Estimated_Revenue = 2000000)
 #'
 #'@export
 
 Gross_Margin <- function(Total_Estimated_Revenue) {
-  Goods_Cost <- Cost_of_Goods_Sold(Total_Estimated_Revenue = Total_Estimated_Revenue)
-  Total_Estimated_Revenue - Goods_Cost
+
+  ifelse(Total_Estimated_Revenue < 500000, stop("error: no data for this revenue range"),
+      percentage <- ifelse(Total_Estimated_Revenue < 999999.99, .2443,
+                          ifelse(Total_Estimated_Revenue < 2499999.99, 2416,
+                                ifelse(Total_Estimated_Revenue < 4999999.99, .2184,
+                                      ifelse(Total_Estimated_Revenue < 24999999.99, .2418, .2321)))))
+
+  if (Total_Estimated_Revenue > 500000) {
+    Total_Estimated_Revenue * percentage
+  }
 }
+
 
