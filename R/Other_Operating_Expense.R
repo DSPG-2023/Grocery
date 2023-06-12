660320dc85bc1352c9c8a5cda8ab2057d41ee684
Aaron N <null94@iastate.edu>
2023-06-12 13:22:18 -0500

    updating with recently found bizminer percentages

--- R/Other_Operating_Expense.R
+++ R/Other_Operating_Expense.R
@@ -1,14 +1,23 @@
 #'Calculation of other operating expenses (miscellaneous)
 #'@param Total_Estimated_Revenue Output of total estimated revenue calculation sheet
-#'@param Percentage Percentage of total estimated revenue spent on other operating expenses. Default value is .1230 (needs clarification)
+#'@param Percentage Percentage of total estimated revenue spent on other operating expenses.
+#'Percentages are taken from Bizminer.
 #'@details This function is taken from the "Estimating_Expenses.xlsx" sheet and calculated in Step Six)
 #'@return Outputs other operating expenses (in dollars)
 #'@examples
-#'# Employee_Wages(Total_Estimated_Revenue = 2000000, Percentage = 0.123)
+#'# Employee_Wages(Total_Estimated_Revenue = 2000000)
 #'
 #'@export
 
-Other_Operating_Expense <- function(Total_Estimated_Revenue, Percentage = .123) {
+Other_Operating_Expense <- function(Total_Estimated_Revenue) {
+
+  ifelse(Total_Estimated_Revenue < 500000, stop("error: no data for this revenue range"),
+         percentage <- ifelse(Total_Estimated_Revenue < 999999.99, .11,
+                              ifelse(Total_Estimated_Revenue < 2499999.99, .12,
+                                     ifelse(Total_Estimated_Revenue < 4999999.99, .11,
+                                            ifelse(Total_Estimated_Revenue < 24999999.99, .11, .10)))))
+
+
   Total_Estimated_Revenue * Percentage
 }
 
