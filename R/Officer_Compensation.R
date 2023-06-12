d51fc80a14b0521c8a4a9b743b38f6c37533f70d
Aaron N <null94@iastate.edu>
2023-06-12 13:01:53 -0500

    rounding bizminer percentages

--- R/Officer_Compensation.R
+++ R/Officer_Compensation.R
@@ -1,6 +1,6 @@
 #'Calculation of Officer Compensation
 #'@param Total_Estimated_Revenue Output of total estimated revenue calculation sheet
-#'@details This function was taken from the "Estimateing_Expenses.xlsx" sheet in Step 6.
+#'@details This function was taken from the "Estimating_Expenses.xlsx" sheet in Step 6.
 #'@return Outputs dollars spent (Total Estimated Revenue * Percentage) (in dollars)
 #'@examples
 #'# Officer_Compensation(Total_Estimated_Revenue = 2000000)
@@ -10,10 +10,10 @@
 
 Officer_Compensation <- function(Total_Estimated_Revenue) {
   ifelse(Total_Estimated_Revenue < 500000, stop("error: no data for this revenue range"),
-         percentage <- ifelse(Total_Estimated_Revenue < 999999.99, .0278,
-                              ifelse(Total_Estimated_Revenue < 2499999.99, .0103,
-                                     ifelse(Total_Estimated_Revenue < 4999999.99, .0158,
-                                            ifelse(Total_Estimated_Revenue < 24999999.99, .0687, .031)))))
+         percentage <- ifelse(Total_Estimated_Revenue < 999999.99, .03,
+                              ifelse(Total_Estimated_Revenue < 2499999.99, .02,
+                                     ifelse(Total_Estimated_Revenue < 4999999.99, .02,
+                                            ifelse(Total_Estimated_Revenue < 24999999.99, .07, .04)))))
 
 
 
