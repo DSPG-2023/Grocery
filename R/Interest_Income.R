a06cbf0225772e31cf89f93290057daaa41bf6e0
Aaron N <null94@iastate.edu>
2023-06-12 14:48:12 -0500

    fixed percentages

--- R/Interest_Income.R
+++ R/Interest_Income.R
@@ -10,10 +10,10 @@
 Interest_Income <- function(Total_Estimated_Revenue) {
 
   ifelse(Total_Estimated_Revenue < 500000, stop("error: no data for this revenue range"),
-         percentage <- ifelse(Total_Estimated_Revenue < 999999.99, .01,
-                              ifelse(Total_Estimated_Revenue < 2499999.99, .02,
-                                     ifelse(Total_Estimated_Revenue < 4999999.99, .1,
-                                            ifelse(Total_Estimated_Revenue < 24999999.99, .01, .04)))))
+         percentage <- ifelse(Total_Estimated_Revenue < 999999.99, .0001,
+                              ifelse(Total_Estimated_Revenue < 2499999.99, .0002,
+                                     ifelse(Total_Estimated_Revenue < 4999999.99, .001,
+                                            ifelse(Total_Estimated_Revenue < 24999999.99, .0001, .0004)))))
 
   Total_Estimated_Revenue * percentage
 }
