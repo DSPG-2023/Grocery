1337f6f9804798cd175018a550b4071ea435c283
alexcory <alexcory@iastate.edu>
2023-06-22 11:32:10 -0500

    updated Rural_Population *This is broken currently*

--- R/Rural_Population.R
+++ R/Rural_Population.R
@@ -1,17 +1,26 @@
 #'Calculation for a quarter circle.
 #'
 #'@param county_pop the population of the whole county
-#'@param towns_pop the sum of all the towns populations in the county
+#'@param county the name of the county (?)
 #'@param pct_county the percent of the county in the area
 #'@return outputs the rural population
 #'@examples
-#'Rural_Population(18000,9786,17.7)
+#'Rural_Population(18000,"sac",17.7)
 #'@export
 
 
 
-Rural_Population<-function(county_pop, towns_pop, pct_county) {
+Rural_Population<-function(county_pop, county, pct_county) {
   #This function subtracts the sum of all the towns populations from the county and multiplies it
   #by the percent of the county that the area covers to find the # of rural people there are
-  return(floor((county_pop - towns_pop)*pct_county/100))
+
+
+  return(floor((county_pop - sum(tidycensus::get_decennial(get_cities_in_county(county)))*pct_county/100)))
+
+
+
+
+
+
+
 }
