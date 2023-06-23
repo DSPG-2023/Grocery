f442565fd67f0741e154b6563c244ea474f46674
Jay Maxwell <60592508+OnoLocoMoco@users.noreply.github.com>
2023-06-23 15:57:58 -0500

    Update Geography_Identifier.R

--- Test/Geography_Identifier.R
+++ Test/Geography_Identifier.R
@@ -1,90 +1,331 @@
-#install.packages("REAT")
+#'@author Jay Maxwell
+#' Identifies States, Counties, and Cities from a given set of points.
 
-library(tidycensus)
-library(tidyverse)
 library(sf)
-library(REAT)
-
-# BASIC Reilly Example ----
-# https://rdrr.io/cran/REAT/man/reilly.html
-# Example from Converse (1949):
-reilly (39851, 37366, 27, 25)
-# two cities (pop. size 39.851 and 37.366) 
-# with distances of 27 and 25 miles to intermediate town
-myresults <- reilly (39851, 37366, 27, 25)
-myresults$prop_A
-# proportion of location a
-# Distance decay parameter for the given sales relation:
-reilly (39851, 37366, 27, 25, gamma = 1, lambda = NULL, relation = 0.9143555)   
-# returns 2
+library(dplyr)
+library(tigris)
+options(tigris_use_cache=TRUE)
 
 
-# Iowa Reailly Example ----
-# Load population + geometry for all Iowa towns. Note the last parameters which
-# allow usage of higher-resolution TIGER/Line data instead of cartographic
-# boundary data
-ia_towns <- get_decennial(geography = "place",
-                          state="Iowa",
-                          variables  = "DP1_0001C",
-                          sumfile = "dp",
-                          output = "wide",
-                          geometry = TRUE,
-                          cb=FALSE,
-                          keep_geo_vars = TRUE
-)
-
-ames_pop <- ia_towns %>% filter(NAME.x =="Ames") %>% pull(DP1_0001C)
-ames_pop
-
-mtown_pop <- ia_towns %>% filter(NAME.x =="Marshalltown") %>% pull(DP1_0001C)
-mtown_pop
-
-# Let's use State Center as the intermediate town. 
-
-ia_towns %>% filter(NAME.x =="Ames") %>% st_centroid() -> pt_ames
-ia_towns %>% filter(NAME.x == "Marshalltown") %>% st_centroid() -> pt_mtown
-ia_towns %>% filter(NAME.x == "State Center") %>% st_centroid() -> pt_stateCenter
-
-distA <- as.numeric(st_distance(pt_ames, pt_stateCenter))
-distA
-
-distB <- as.numeric(st_distance(pt_mtown, pt_stateCenter))
-distB
-
-reilly(ames_pop, mtown_pop, distA, distB)
-
-# The closer proximity to State Center implies that MTown gets more retail 
-# trade from State Center than does Ames, despite Ames' larger 
-# population/actractivity
-
-
-
-# Custom Reilly Example ----
-# Do do this in a function....
-
-custom_reilly <- function(a, b, c) {
-  # input: three Iowa town names
-  # assumptions: a data frame of ia_towns already in memory
+get_county <- function(x) {
+  #input: a single-row data frame, columns 1, 2 = lon, lat
+  #                                column 3 = state name
+  #output: x with extra column for county name
+  counties <- counties(state=x[["state"]])
   
-  #print(paste0("Inputs: ", a, ", ", b, ", ", c))
-  ia_towns %>% filter(NAME.x == a) %>% st_centroid() -> pt_a
-  ia_towns %>% filter(NAME.x == b) %>% st_centroid() -> pt_b
-  ia_towns %>% filter(NAME.x == c) %>% st_centroid() -> pt_c
+  point <- st_as_sf(x, coords = c("lon", "lat"), crs=st_crs(counties))
   
-  pop_a <- pt_a %>% pull(DP1_0001C)
-  pop_b <- pt_b %>% pull(DP1_0001C)
-  
-  dist_a <- as.numeric(st_distance(pt_a, pt_c))
-  dist_b <- as.numeric(st_distance(pt_b, pt_c))
-  
-  
-  
-  reilly(pop_a, pop_b, dist_a, dist_b)
-  
+  point %>%
+    st_join(counties, join=st_intersects) %>%
+    mutate(county = NAME) %>%
+    select(county) %>%
+    st_drop_geometry() -> county
+
+    return(cbind(x, county) )
 }
 
-# This needs to be run prior to the function call
-# alternatively, this could be placed inside the function call
+
+
+
+get_city <- function(x) {
+  # input: a single-row data frame, columns 1, 2 = lon, lat
+  #                                column 3 = state name
+  # (does not require county column)
+  # output: x with extra column for city name
+
+  cities <- places(state=x[["state"]])
+  
+  point <- st_as_sf(x, coords = c("lon", "lat"), crs=st_crs(cities))
+  
+  point %>%
+    st_join(cities, join=st_intersects) %>%
+    mutate(city = NAME) %>%
+    select(city) %>%
+    st_drop_geometry() -> city
+  
+  return(cbind(x, city) )
+}
+
+
+get_cities_in_county <- function(x) {
+  #input: a single-row data frame, a column named "state"
+  #                                a column named "county"
+  #output: x with extra column for all the cities w/in a county
+  
+  state <- x[["state"]]
+  county <- x[["county"]]
+  
+  # Get TIGER/Line geometry for all the counties and cities/places
+  # in our state of interest
+  cities <- places(state=state)
+  counties <- counties(state=state)
+  
+  counties %>% 
+    filter(NAME == county) %>% 
+    st_join(cities, join=st_intersects) %>% 
+    pull(NAME.y) %>% 
+    sort() %>% 
+    paste(collapse = ", ") -> city_list
+  
+  return(cbind(x, city_list))
+  
+} 
+
+# Inititial Setup ----
+
+# Get states geographic data using TIGRIS, 
+# Use TIGER/Line, not cb (cartographic boundary) files for best 
+# resolution / results
+states <- states()
+plot(states$geometry)
+
+# Make a layer with some random/fake coordinate
+points <- data.frame(lon=c(-93, -92.9062656, -93.635049, -93, -93), lat=c(42, 42.0342997, 42.001649, 45, 33))
+points <- data.frame(lon=c(-95.0520959), lat=c(42.3056645))
+points
+
+# Convert the points data frame into a spatial object
+points_sf <- st_as_sf(points, coords = c("lon", "lat"), crs=st_crs(states))
+points_sf
+
+
+
+# Get the State ----
+
+states_vector <- points_sf %>%
+  st_join(states, join=st_intersects) %>%
+  mutate(state = NAME) %>%
+  select(state) %>%
+  st_drop_geometry()
+
+states_df <- cbind(points, states_vector)
+
+states_df
+
+# Get the County ----
+# How can we find out which COUNTY a point lies in?
+# Assumption: We already know what STATE it is in
+
+# A new data frame to hold points + state + county
+counties_df <- data.frame()
+
+
+# Loop through every row in states_df, adds a column for county
+for(i in 1:nrow(states_df)) {
+  counties_df <- rbind(counties_df, get_county(states_df[i,]))  
+}
+
+# We now know which county each point lies in
+counties_df
+
+
+
+# Get the city ----
+# A new data frame to holds points + state + county + city
+city_df <- data.frame()
+
+# Loop through every row in county_df, add a column for city
+for(i in 1:nrow(counties_df)) {
+  city_df <- rbind(city_df, get_city(counties_df[i,]))  
+}
+
+# This data frame now tells us what CITY the dot lies in
+city_df
+
+
+
+# Get all cities in county ----
+# A new data frame to hold points + state + county + city + all the cities in the county
+list_of_cities_df <- data.frame()
+
+# Loop through every row in city_df, adding a column for the list of cities in the county
+# this does not require the existence of the city column to function. 
+# I am just doing this in a sequence
+for(i in 1:nrow(city_df)) {
+  list_of_cities_df <- rbind(list_of_cities_df, get_cities_in_county(city_df[i,]))  
+}
+
+# This data frame reads ugly in the console
+list_of_cities_df
+
+# Easier to read in View
+View(list_of_cities_df)
+
+
+# Using the distances in the RDS file from gitHub ----
+d <- readRDS("dspg/distance_latlong_test.Rds")
+
+# Convert the points data frame into a spatial object
+d_sf <- st_as_sf(d, coords = c("lon", "lat"), crs=st_crs(states))
+d_sf
+
+
+d_states <- d_sf %>%
+  st_join(states, join=st_intersects) %>%
+  mutate(state = NAME) %>%
+  select(state) %>%
+  st_drop_geometry()
+
+
+d_states <- cbind(d, d_states)
+d_states
+
+
+
+d_counties <- data.frame()
+
+
+# Loop through every row in states_df, adds a column for county
+for(i in 1:nrow(d_states)) {
+  d_counties <- rbind(d_counties, get_county(d_states[i,]))  
+}
+
+d_counties
+
+
+d_cities <- data.frame()
+for(i in 1:nrow(counties_df)) {
+  d_cities <- rbind(d_cities, get_city(d_counties[i,]))  
+}
+
+d_cities
+
+d_city_list <- data.frame()
+
+# Loop through every row in city_df, adding a column for the list of cities in the county
+# this does not require the existence of the city column to function. 
+# I am just doing this in a sequence
+for(i in 1:nrow(city_df)) {
+  d_city_list <- rbind(d_city_list, get_cities_in_county(d_cities[i,]))  
+}
+
+# This data frame reads ugly in the console
+d_city_list
+
+View(d_city_list)
+
+
+
+# -----------------------------------------------------------------------------
+# Working with Geographic Areas ----
+
+# Get all counties using tigris
+ia_counties <- counties(state = "Iowa")
+
+# These plot statements are equivalent
+plot(st_geometry(ia_counties))
+ia_counties %>% st_geometry() %>% plot()
+
+# Union all the counties to make a single feature
+ia <- st_union(ia_counties)
+ia   
+ia %>% st_geometry() %>% plot()
+
+
+# Filter four counties from central IA, union their geometry into a 
+# single polygon
+four_county_area <- ia_counties %>% 
+  filter(NAME %in% c("Polk", "Dallas", "Boone", "Story")) %>% 
+  st_union() %>% 
+  st_sf()
+
+
+# Plot the geometry - does that look like the expected outcome?
+four_county_area %>% st_geometry() %>% plot()
+
+# Plot multiple layers together to view
+plot(st_geometry(ia), lwd=2)
+plot(st_geometry(ia_counties), add=TRUE)
+plot(st_geometry(four_county_area), col='red', add=TRUE)
+
+
+# Let's make a three-sided polygon using the centroids of three Iowa
+# counties as vertices
+
+# First, filter the counties, find their centroids, extract the coordinates
+tri_county_coords <- ia_counties %>% 
+  filter(NAME %in% c("Tama", "Delaware", "Henry")) %>% 
+  st_centroid() %>% 
+  st_coordinates()
+
+# These are the centroids of the counties selected above
+tri_county_coords
+
+# In order to make a closed polygon, we need to repeat the first & last coord
+tri_county_coords <- rbind(tri_county_coords, tri_county_coords[1,])
+tri_county_coords
+
+# Turn the coordinates into a list, pass it to st_polygon,
+# turn that into an sfc object, then turn that into an sf object
+triangle <- tri_county_coords %>% 
+  list() %>% 
+  st_polygon() %>% 
+  st_sfc(crs = st_crs(ia_counties)) %>% 
+  st_sf()
+
+# It's a triangle!
+plot(st_geometry(triangle))
+
+class(triangle)
+st_crs(triangle)
+
+# Plot multiple layers together to view
+plot(st_geometry(ia), lwd=2)
+plot(st_geometry(ia_counties), add=TRUE)
+plot(st_geometry(four_county_area), col='red', add=TRUE)
+plot(st_geometry(triangle), col='green', add=TRUE)
+
+
+
+# One last area, using a buffered point
+
+# First, filter the counties, find their centroids, extract the coordinates
+cherokee_centroid <- ia_counties %>% 
+  filter(NAME =="Cherokee") %>% 
+  st_centroid()
+
+# I am not sure what unit these are -- meters, maybe?
+cherokee_buffer <- cherokee_centroid %>% 
+  st_buffer(dist=50000)
+
+# It looks like a circle!
+cherokee_buffer %>% st_geometry() %>% plot()
+
+# Plot multiple layers together to view
+plot(st_geometry(ia), lwd=2)
+plot(st_geometry(ia_counties), add=TRUE)
+plot(st_geometry(four_county_area), col='red', add=TRUE)
+plot(st_geometry(triangle), col='green', add=TRUE)
+plot(st_geometry(cherokee_buffer), col="steelblue", add=TRUE)
+
+
+
+# So now we have three geographic areas that overlay with Iowa counties
+# Let's try to figure out which counties intersect these geometries
+
+cherokee_buffer %>% 
+  st_intersection(ia_counties) %>% 
+  pull(NAME.1)
+
+triangle %>% st_sf() %>% 
+  st_intersection(ia_counties) %>% 
+  pull(NAME)
+
+# Sharing a border counts as an intersect!
+four_county_area %>% 
+  st_intersection(ia_counties) %>% 
+  pull(NAME)
+
+
+
+
+library(tidycensus)
+# Examine the cencus variable available
+#v20 <- load_variables(2020, "dp")
+
+# Load population + geometry for all Iowa towns. Note the last parameters which
+# allow usage of higher-resolution TIGER/Line data instead of cartographic
+
 ia_towns <- get_decennial(geography = "place",
                           state="Iowa",
                           variables  = "DP1_0001C",
@@ -96,55 +337,179 @@ ia_towns <- get_decennial(geography = "place",
 )
 
 
-custom_reilly("Ames", "Marshalltown", "Nevada")
-custom_reilly("Waterloo", "Cedar Rapids", "Vinton")
 
 
-# HUFF ----
 
-## Huff Example 1 ----
-# https://rdrr.io/cran/REAT/man/huff.html
+# Doing so gives us a way to filter out towns of certain sizes
+pop_cutoff <- 1000
 
-# Example from Levy/Weitz (2009):
+big_towns <- ia_towns %>% filter(DP1_0001C>=pop_cutoff)
+big_towns
 
-# Data for the existing and the new location
-locations <- c("Existing Store", "New Store")
-S_j <- c(5000, 10000)                ## What do these numbers represent??
-location_data <- data.frame(locations, S_j)
-location_data
+big_towns %>% st_geometry() %>% plot()
+
+# What towns are in the geographic areas we have made?
 
 
-# Data for the two communities (Rock Creek and Oak Hammock)
-communities <- c("Rock Creek", "Oak Hammock")
-C_i <- c(5000000, 3000000)           ## What do these numbers represent??
-community_data <- data.frame(communities, C_i)
-community_data
+triangle %>% st_intersection(big_towns) -> triangle_towns
+
+plot(st_geometry(ia), lwd=2)
+plot(st_geometry(ia_counties), add=TRUE)
+plot(st_geometry(triangle), col='green', add=TRUE)
+plot(st_geometry(triangle_towns), col='black', add=TRUE)
 
 
-# Combining location and submarket data in the interaction matrix
-interactionmatrix <- merge (communities, location_data)
-interactionmatrix
+cherokee_buffer %>% st_intersection(big_towns) -> buffer_towns
 
-# Adding driving time:
-interactionmatrix[1,4] <- 10
-interactionmatrix[2,4] <- 5
-interactionmatrix[3,4] <- 5
-interactionmatrix[4,4] <- 15
-colnames(interactionmatrix) <- c("communities", "locations", "S_j", "d_ij")
-
-interactionmatrix
+plot(st_geometry(cherokee_buffer), col="steelblue", add=TRUE)
+plot(st_geometry(buffer_towns), col='black', add=TRUE)
 
 
-huff_shares <- huff(interactionmatrix, "communities", "locations", "S_j", "d_ij")
-huff_shares
-# Market shares of the new location:
-huff_shares$ijmatrix[huff_shares$ijmatrix$locations == "New Store",]
+
+four_county_area %>% st_intersection(big_towns) -> four_county_towns
+
+plot(st_geometry(four_county_area), col='red', add=TRUE)
+plot(st_geometry(four_county_towns), col='black', add=TRUE)
+
+# There is a small problem with this output, though... Look at the red counties...
+# The city of Des Moines does not stop at the southern county line--the city
+# limits go past that. However, our geometry is only the INTERSECTION of the 
+# four-county area & big towns (Des Moines, etc.). If we want to accurately plot
+# the towns that are not fully within an area, we need to find another method. 
+# But this is a good start!!
+#
+# (This might be happening in the green triangle, too. But I don't know my eastern
+# Iowa cities well enough to pick out if anything is being clipped.)
+
+# Make a vector of just the town names, no geometry
+four_county_towns %>% pull(NAME.x) -> four_county_towns_names
+four_county_towns_names
+
+# Add a new layer to the plot of the towns from the list above
+ia_towns %>% 
+  filter(NAME.x %in% four_county_towns_names) %>% 
+  st_geometry() %>% 
+  plot(col="black", add=TRUE)
+
+# Now the southern portion of Des Moines, which falls outside of the red zone,
+# will display properly. We should probably do the same for the other areas, 
+# in case there some boundary-crossing towns, but for this example, it's good 
+# enough. 
 
 
-huff_all <- huff(interactionmatrix, "communities", "locations", "S_j", "d_ij",
-                 localmarket_dataset = community_data, origin_id = "communities", localmarket = "C_i")
+# Voronoi Polygons ----
 
-huff_all
+# First example found here:
+# https://gis.stackexchange.com/questions/362134/i-want-to-create-a-voronoi-diagram-while-retaining-the-data-in-the-data-frame
 
-huff_all$totals
+st_voronoi_point <- function(points){
+  ## points must be POINT geometry
+  # check for point geometry and execute if true
+  if(!all(st_geometry_type(points) == "POINT")){
+    stop("Input not  POINT geometries")
+  }
+  g = st_combine(st_geometry(points)) # make multipoint
+  v = st_voronoi(g)
+  v = st_collection_extract(v)
+  return(v[unlist(st_intersects(points, v))])
+}
+
+p = st_as_sf(data.frame(x=runif(10),y=runif(10),A=letters[1:10]),coords=1:2)
+v = st_voronoi_point(p)
+
+p
+v
+
+class(p)
+class(v)   
+# Note the different class types between the two objects
+# As best as I can tell, sfc is a more primtive type of spatial object
+# It can have a CRS, but only has geometry -- there is no attribute data
+# attached to the sfc. But an sfc can be turned into an sf object
+
+p %>% st_geometry() %>% plot()
+v %>% st_geometry() %>% plot(add=TRUE)
+
+v[[1]] %>% st_geometry() %>% plot(col='red', add=TRUE)
+p[1,] %>% st_geometry() %>% plot(col='blue', pch=3,add=TRUE)
+
+# This is not working  :-(
+# I think it is supposed to apply the geometry of v to the object p
+pv = st_set_geometry(p, v)
+pv
+
+# Can we make a voronoi diagram from the centroids of the Iowa counties?
+
+# Quick exam of the ia_counties layer
+glimpse(ia_counties)
+class(ia_counties)
+
+# get county centroids
+p_counties <- ia_counties %>% st_centroid()
+
+# Plot centroids for inspection
+plot(st_geometry(ia), lwd=2)
+plot(st_geometry(ia_counties), add=TRUE)
+plot(st_geometry(p_counties), pch=3, add=TRUE)
+
+# use the custom function to turn centroids into voronoi polygons
+# this produces an error message, but the results look fairly accurate?
+v_counties <- st_voronoi_point(p_counties)
+
+# Visualize the results
+plot(ia)
+plot(st_geometry(p_counties), pch=3, add=TRUE)
+plot(v_counties, add=TRUE)
+
+
+
+# Can we make a Voronoi output for CITIES of a certain POPULATION?
+
+pop_cutoff <- 2000
+
+big_towns <- ia_towns %>% filter(DP1_0001C>=pop_cutoff)
+big_towns
+
+
+plot(ia)
+big_towns %>% st_geometry() %>% plot(add=TRUE)
+
+# Make voronoi polygon layers
+v_big_towns <- big_towns %>% st_centroid() %>% st_voronoi_point()
+
+
+# Let's plot and see how it looks
+plot(ia)
+plot(v_big_towns, add=TRUE)
+plot(big_towns %>% st_centroid(), pch=3, col='red', add=TRUE)
+
+# That looks pretty good.
+# Can we get rid of the voronoi lines that extend beyond Iowa's border?
+
+v_big_towns_clipped <- st_intersection(v_big_towns, ia)
+
+plot(v_big_towns_clipped)
+plot(big_towns %>% st_centroid(), pch=3, col='red', add=TRUE)
+# Remember, the crosses are not the centroid of the voronoi polygon
+# the cross is the location of a TOWN with a certain population size
+
+
+
+# Bailey asked about towns with less than 2500 populatoin
+# because there are a lot of towns in that catagory, there will be 
+# a lot of voronoi polygons
+
+pop_cutoff2 <- 2500
+
+small_towns <- ia_towns %>% filter(DP1_0001C<pop_cutoff2)
+v_small_towns <- small_towns %>% st_centroid() %>% st_voronoi_point()
+
+# Here is a list of all the towns less than 2500
+small_towns %>% pull(NAME.x)
+
+# Plot
+plot(ia)
+plot(v_small_towns, add=TRUE)
+plot(small_towns %>% st_centroid(), pch=3, col='red', add=TRUE)
+
 
