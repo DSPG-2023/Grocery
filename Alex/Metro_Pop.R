75ae0783f7c2317272880e921a9eb195ee296196
alexcory <alexcory@iastate.edu>
2023-06-29 08:19:55 -0500

    Added code for Metro_Pop. Forgot to save before last push

--- Alex/Metro_Pop.R
+++ Alex/Metro_Pop.R
@@ -1,5 +1,8 @@
 
 
-Metro_Pop <- function()
+Metro_Pop <- function(address) {
   #We should be saving address as a global variable so we don't have to pass it
   #as a parameter in most of my functions
+  df %>% filter(City == splt_addr["city"])
+  return(df$value)
+}
