d3a7338099305b9dd9d166ad0f3a11660fe7e0e0
Aaron N <null94@iastate.edu>
2023-07-07 13:52:55 -0500

    Other operating expenses documentation update

--- R/Other_Operating_Expense.R
+++ R/Other_Operating_Expense.R
@@ -1,13 +1,28 @@
-#'Calculation of other operating expenses (miscellaneous)
-#'@param Total_Estimated_Revenue Output of total estimated revenue calculation sheet
-#'@param Percentage Percentage of total estimated revenue spent on other operating expenses
-#'(default from Bizminer)
-#'@details This function is taken from the "Estimating_Expenses.xlsx" sheet and calculated in Step Six)
-#'@return Outputs other operating expenses (in dollars)
-#'@examples
-#'# Employee_Wages(Total_Estimated_Revenue = 2000000, Percentage = .1088)
+#' Calculates the Estimated Cost of Miscellaneous Expenses for Grocery Stores
 #'
-#'@export
+#' @author Aaron Null
+#'
+#' @description
+#' This function calculates the estimated cost of miscellaneous expenses for a
+#' hypothetical store in a user-selected location based off of a user-selected percentage
+#' parameter. If not specified by the user, this percentage defaults to the industry average
+#' provided by Bizminer. These expenses include tax, administrative and repair costs,
+#' among others.
+#'
+#'
+#' @details
+#' This function employs a default percentage value from the financial analysis
+#' and market research firm Bizminer and is based upon calculations originally
+#' formulated by FFED ISU Extension and Outreach.
+#'
+#' @param Total_Estimated_Revenue Total estimated revenue based on market size/location.
+#' @param Percentage Percentage of total estimated revenue spent on miscellaneous
+#'("other") operating expenses selected via user input (default from Bizminer).
+#'
+#' @returns The output returns the estimated dollar amount spent on these other expenses for
+#' a hypothetical grocery store in a given location.
+#'
+#' @export
 
 Other_Operating_Expense <- function(Total_Estimated_Revenue, Percentage = .1088) {
 
