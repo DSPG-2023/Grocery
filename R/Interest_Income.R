52f9d404c087f626a25279ac5db7f32302913cfa
Aaron N <null94@iastate.edu>
2023-07-07 14:10:23 -0500

    interest income documentation update

--- R/Interest_Income.R
+++ R/Interest_Income.R
@@ -1,15 +1,25 @@
-#' Calculation of Interest Income
+#' Calculates the Estimated Income of from Interest-Bearing Assets for Grocery Stores
 #'
-#' @param Total_Estimated_Revenue Calculation of Total_Estimated Revenue
-#' @param Percentage Percentage of revenue of the estimated income from interest
+#' @author Aaron Null
 #'
-#' @details This function is taken from the "Estimating_Expenses.xlsx" sheet and
-#' calculated in Step Eight.
+#' @description
+#' This function calculates the estimated income from interest-bearing assets
+#' for a hypothetical store in a user-selected location based off of a user-selected
+#' percentage parameter. If not specified by the user, this percentage defaults
+#' to the industry average provided by Bizminer.
 #'
-#' @returns Outputs first-year interest income (in dollars)
 #'
-#' @examples
-#' Interest_Income(Total_Estimated_Revenue = 2000000, Percentage = .0004)
+#' @details
+#' This function employs a default percentage value from the financial analysis
+#' and market research firm Bizminer and is based upon calculations originally
+#' formulated by FFED ISU Extension and Outreach.
+#'
+#' @param Total_Estimated_Revenue Total estimated revenue based on market size/location.
+#' @param Percentage Percentage of total estimated revenue that constitutes the added
+#' income from interest selected via user input (default from Bizminer).
+#'
+#' @returns The output returns the estimated dollar amount earned as income from interest for
+#' a hypothetical grocery store in a given location.
 #'
 #' @export
 
