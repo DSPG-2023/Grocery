5d0d427bd74b8744ee226dc7bf90bc7420fdf9a9
HarunCelikOtto <hcelik@iastate.edu>
2023-07-07 21:42:33 -0500

    Function Calc_Counties_Pop.R drafted, requires documentation.

---
+++ Harun/testR/Calc_Counties_Pop.R
@@ -0,0 +1,23 @@
+Calc_Counties_Pop <- function(states_unique = states_unique) {
+  all_counties_pops <- NULL
+
+
+  for (index_state in 1:length(states_unique)) {
+
+    df_census_subset <- df_census_call %>%
+      filter(state == states_unique[index_state])
+
+    counties_unique <- unique(df_census_subset$counties)
+
+
+    counties_populations <- get_decennial(year = 2020,
+                                          geography = "county",
+                                          variables = "DP1_0001C",
+                                          sumfile = "dp",
+                                          county = counties_unique,
+                                          state = states_unique[index_state])
+
+    all_counties_pops <- rbind(all_counties_pops, counties_populations)
+  }
+  return(all_counties_pops)
+}
