c94343d6c938f67cb4e2551e9b31941e767ce158
alexcory <alexcory@iastate.edu>
2023-07-10 11:14:16 -0500

    Metro_Pop Finalized

--- Alex/Metro_Pop.R
+++ Alex/Metro_Pop.R
@@ -2,10 +2,10 @@
 #' @description Returns the population of the city you are currently in
 #' @param address the address we are interested in.
 
-Metro_Pop <- function(address) {
+Metro_Pop <- function(address, df_city_pop) {
 
 
   splt_addr <- Address_Parser(address)
-  df <- df %>% filter(City == splt_addr[["city"]])
+  df <- df_city_pop %>% filter(City == splt_addr[["city"]])
   return(df$value)
 }
