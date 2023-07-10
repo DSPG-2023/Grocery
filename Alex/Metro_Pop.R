2b73091093ef4fd2c335368ee60af9e651540eff
alexcory <alexcory@iastate.edu>
2023-07-10 11:18:43 -0500

    Edited Documentation

--- Alex/Metro_Pop.R
+++ Alex/Metro_Pop.R
@@ -1,6 +1,8 @@
 #' @author Alex Cory
 #' @description Returns the population of the city you are currently in
 #' @param address the address we are interested in.
+#' @param df_city_pop dataframe with population of cities in county
+#' @return population of city the store is in.
 
 Metro_Pop <- function(address, df_city_pop) {
 
