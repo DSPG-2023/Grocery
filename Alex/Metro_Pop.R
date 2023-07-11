97840f53d74c60950106df4721253e498f40812c
alexcory <alexcory@iastate.edu>
2023-07-11 12:51:22 -0500

    Added Documentation

--- Alex/Metro_Pop.R
+++ Alex/Metro_Pop.R
@@ -1,8 +1,11 @@
+#' Finds the Population of the City the Store is in.
 #' @author Alex Cory
 #' @description Returns the population of the city you are currently in
 #' @param address the address we are interested in.
 #' @param df_city_pop dataframe with population of cities in county
 #' @return population of city the store is in.
+#' @importFrom stringr str_to_title
+#' @export
 
 Metro_Pop <- function(address, df_city_pop) {
   splt_addr <- Address_Parser(address)
