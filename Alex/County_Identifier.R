402482a006d582a9f5ba477d08a0c7c4f286aff9
alexcory <alexcory@iastate.edu>
2023-07-11 12:18:17 -0500

    Added Documentation

--- Alex/County_Identifier.R
+++ Alex/County_Identifier.R
@@ -1,7 +1,13 @@
+#' Find County from Long/Lat points and state
+#' @author Jay Maxwell
+#' @param x Single row data frame,columns 1, 2 = lon, lat, column 3 = state name
+#' @return x with extra column for county name
+#' @importFrom dplyr mutate select
+#' @importFrom magrittr ?>?
+#' @importFrom sf st_join st_drop_geometry st_intersects st_as_sf st_crs
+#' @importFrom tigris counties
+#' @export
 get_county <- function(x) {
-  #input: a single-row data frame, columns 1, 2 = lon, lat
-  #                                column 3 = state name
-  #output: x with extra column for county name
   counties <- counties(state=x[["state"]])
 
   point <- st_as_sf(x, coords = c("lon", "lat"), crs=st_crs(counties))
@@ -17,27 +23,15 @@ get_county <- function(x) {
 
 
 
-
-get_city <- function(x) {
-  # input: a single-row data frame, columns 1, 2 = lon, lat
-  #                                column 3 = state name
-  # (does not require county column)
-  # output: x with extra column for city name
-
-  cities <- places(state=x[["state"]])
-
-  point <- st_as_sf(x, coords = c("lon", "lat"), crs=st_crs(cities))
-
-  point %>%
-    st_join(cities, join=st_intersects) %>%
-    mutate(city = NAME) %>%
-    select(city) %>%
-    st_drop_geometry() -> city
-
-  return(cbind(x, city) )
-}
-
-
+#' Find all Cities in a County Given State and County
+#' @author Jay Maxwell
+#' @param x a single-row data frame, a column named "state" a column named "county"
+#' @return x with all cities in a column as a list
+#' @importFrom dplyr mutate select pullcollapse
+#' @importFrom magrittr ?>?
+#' @importFrom sf st_join  st_intersects
+#' @importFrom tigris counties places
+#' @export
 get_cities_in_county <- function(x) {
   #input: a single-row data frame, a column named "state"
   #                                a column named "county"
